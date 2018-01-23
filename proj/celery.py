b1dc80b67b207d71d0f1c15f76da99ef02a41ba6
rob-deutsch <robzyb+altgithub@gmail.com>
2018-01-23 11:44:34 +1100

    Celery hangs with .s() in setup_periodic_tasks

--- proj/celery.py
+++ proj/celery.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
+from demoapp.tasks import add
 
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'proj.settings')
@@ -16,6 +17,11 @@ app.config_from_object('django.conf:settings', namespace='CELERY')
 # Load task modules from all registered Django app configs.
 app.autodiscover_tasks()
 
+@app.on_after_finalize.connect
+def setup_periodic_tasks(sender, **kwargs):
+    print("Attempting to print add.s()")
+    print(add.s(2,2))
+    print("Printed add.s()")
 
 @app.task(bind=True)
 def debug_task(self):
